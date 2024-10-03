bdc35904df03227360201ff6ae402e842e546251
Sven Lange <sven.lange@hm.edu>
2024-10-03 16:02:53 +0200

    Fixing the worst error of my life.

--- src/test/java/de/kastenklicker/secureserverbackup/CertGenerator.java
+++ src/test/java/de/kastenklicker/secureserverbackup/CertGenerator.java
@@ -22,27 +22,26 @@ import java.util.Date;
 
 public class CertGenerator {
     
-    public static void generate(File keyFile, File certFile) throws Exception {
-        // Schlüsselpaar (RSA) generieren
+    public static void generate(File certFile, File keyFile) throws Exception {
+        // Generate RSA key pair
         java.security.KeyPairGenerator keyPairGenerator = java.security.KeyPairGenerator.getInstance("RSA");
         keyPairGenerator.initialize(2048, new SecureRandom());
         KeyPair keyPair = keyPairGenerator.generateKeyPair();
         PrivateKey privateKey = keyPair.getPrivate();
 
-        // Gültigkeitszeitraum setzen
+        // Set validity for 1 day
         Calendar calendar = Calendar.getInstance();
-        Date startDate = calendar.getTime();  // jetzt
-        // TODO to 1 day
-        calendar.add(Calendar.YEAR, 1);       // Gültigkeit: 1 Jahr
+        Date startDate = calendar.getTime();
+        calendar.add(Calendar.DAY_OF_YEAR, 1);       // 1 day
         Date endDate = calendar.getTime();
 
-        // Zertifikatsinformationen festlegen (DN, Seriennummer, Algorithmus, etc.)
+        // Set certificate information
         X500Name dnName = new X500Name("CN=localhost");
         BigInteger certSerialNumber = new BigInteger(Long.toString(new SecureRandom().nextLong()));
         X509v3CertificateBuilder certBuilder = new JcaX509v3CertificateBuilder(
                 dnName, certSerialNumber, startDate, endDate, dnName, keyPair.getPublic());
 
-        // Erweiterungen hinzufügen (KeyUsage, SAN, etc.)
+        // Add Extensions
         certBuilder.addExtension(
                 Extension.subjectAlternativeName, false,
                 new GeneralNames(new GeneralName(GeneralName.dNSName, "localhost")));
@@ -52,19 +51,22 @@ public class CertGenerator {
                 Extension.extendedKeyUsage, false,
                 new ExtendedKeyUsage(KeyPurposeId.id_kp_serverAuth));
 
-        // Zertifikat mit SHA256 und dem privaten Schlüssel signieren
+        // Sign certificate with private key
         ContentSigner contentSigner = new JcaContentSignerBuilder("SHA256WithRSA").build(privateKey);
-        X509Certificate certificate = new JcaX509CertificateConverter().getCertificate(certBuilder.build(contentSigner));
+        X509Certificate certificate = new JcaX509CertificateConverter()
+                .getCertificate(certBuilder.build(contentSigner));
 
-        // PEM-Dateien schreiben        
+        // Write to pem files      
         try (Writer certWriter = new FileWriter(certFile);
              JcaPEMWriter pemCertWriter = new JcaPEMWriter(certWriter)) {
             pemCertWriter.writeObject(certificate);
+            pemCertWriter.flush();
         }
 
         try (Writer keyWriter = new FileWriter(keyFile);
              JcaPEMWriter pemKeyWriter = new JcaPEMWriter(keyWriter)) {
             pemKeyWriter.writeObject(privateKey);
+            pemKeyWriter.flush();
         }
     }
 }
