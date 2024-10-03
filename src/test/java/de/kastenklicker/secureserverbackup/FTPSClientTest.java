54eee4ef28153e4f1fc9b27c36a688f2b4566318
Sven Lange <sven.lange@hm.edu>
2024-10-03 12:44:40 +0200

    Formal improvements

--- src/test/java/de/kastenklicker/secureserverbackup/FTPSClientTest.java
+++ src/test/java/de/kastenklicker/secureserverbackup/FTPSClientTest.java
@@ -16,18 +16,19 @@ public class FTPSClientTest {
 
     private static String hostname;
     private static int port;
-    private static final String username = "alpineftp";
-    private static final String authentication = "alpineftp"; // password or path of private key file
-    private final String remoteDirectory = "/ftp/alpineftp";
+    private static final String USERNAME = "alpineftp";
+    private static final String AUTHENTICATION = "alpineftp"; // password
+    private static final String REMOTE_DIRECTORY = "/ftp/alpineftp";
+    private static final int PASSIV_PORT = 21000;
     
     @BeforeAll
     public static void BeforeAll() {
         // Create & start Docker Container
         ftpsContainer = new FixedHostPortGenericContainer<>("delfer/alpine-ftp-server:latest")
-                .withFixedExposedPort(21000, 21000)
+                .withFixedExposedPort(PASSIV_PORT, PASSIV_PORT)
                 .withExposedPorts(21)
-                .withEnv("MIN_PORT", "21000")
-                .withEnv("MAX_PORT", "21000")
+                .withEnv("MIN_PORT", String.valueOf(PASSIV_PORT))
+                .withEnv("MAX_PORT", String.valueOf(PASSIV_PORT))
                 .withEnv("TLS_CERT", "/letsencrypt/cert.pem")
                 .withEnv("TLS_KEY", "/letsencrypt/key.pem")
                 .withCopyFileToContainer(
@@ -41,19 +42,23 @@ public class FTPSClientTest {
         hostname = ftpsContainer.getHost();
         port = ftpsContainer.getMappedPort(21);
     }
-    
+
+    /**
+     * Checks if the upload is working fine
+     * @throws Exception FTPS Exceptions
+     */
     @Test
     public void testUpload() throws Exception {
         
-        final FTPSClient ftpsClient = new FTPSClient(hostname, port, username,
-                authentication, null, 0, remoteDirectory);
+        final FTPSClient ftpsClient = new FTPSClient(hostname, port, USERNAME,
+                AUTHENTICATION, null, 0, REMOTE_DIRECTORY);
         
          ftpsClient.upload(
                 new File("./src/test/resources/zipTest/test.txt"));
 
         // Check if file was transferred correctly
         File testFile = new File("./src/test/resources/testUpload.txt");
-        ftpsContainer.copyFileFromContainer("/ftp/alpineftp/test.txt", testFile.getPath());
+        ftpsContainer.copyFileFromContainer(REMOTE_DIRECTORY + "/test.txt", testFile.getPath());
         assertTrue(testFile.delete());
     }
 
